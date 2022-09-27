a57653cdc137172872f0eb11fc31a837cc0aa663
Kornello <36538151+KirillKotenok@users.noreply.github.com>
2022-09-27 13:10:35 +0300

    Delete src/main/resources directory

--- src/main/resources/db/migration/V1__create_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS shortened_urls
-(
-    short_url    TEXT NOT NULL,
-    original_url TEXT NOT NULL,
-    title        TEXT NOT NULL,
-    created_at   TIMESTAMP default now(),
-    CONSTRAINT shortened_urls_PK PRIMARY KEY (short_url),
-    CONSTRAINT shortened_urls_UK UNIQUE (original_url)
-);
