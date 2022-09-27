f57fdd121ae24e0a45c42b288aec2d9c5064b5f9
Kornello <36538151+KirillKotenok@users.noreply.github.com>
2022-09-27 13:10:26 +0300

    Delete src/main/java/com/kornello/url_shortener_service directory

--- src/main/java/com/kornello/url_shortener_service/service/ShortenerService.java
+++
@@ -1,8 +0,0 @@
-package com.kornello.url_shortener_service.service;
-
-public interface ShortenerService {
-
-    String shortUrl(String json);
-
-    String getOriginalUrlFromShortUrl(String shortUrl);
-}
